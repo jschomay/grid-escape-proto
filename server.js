3ccfdc29662f3c882b27e03fce938a1075f0d05f
jschomay <jeffpublic@gmail.com>
2013-04-05 14:49:27 -0700

    set up for browswerify-middleware

--- server.js
+++ server.js
@@ -1,34 +1,12 @@
-/*
- * Module dependencies
- */
-var express = require('express'),
-  // stylus = require('stylus'),
-  // nib = require('nib'),
-  app = express();
+// using https://github.com/ForbesLindesay/browserify-middleware
+var browserify = require('browserify-middleware');
+var express = require('express');
+var app = express();
 
-// function compile(str, path) {
-//   return stylus(str)
-//     .set('filename', path)
-//     // .set('compress', true)
-//     .use(nib());
-// }
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'jade');
+//provide browserified versions of all the files in a directory
+app.use('/src', browserify('crafty/app/src'));
+
+app.use(express.static(__dirname + '/crafty/app'));
 app.use(express.logger('dev'));
-// app.use(stylus.middleware(
-//   { src: __dirname + '/public',
-//     compile: compile
-//   }
-// ));
-app.use(express.static(__dirname + '/'));
-
-// app.get('/', function (req, res) {
-//   res.render('index',
-//   { title : 'Home' }
-//   );
-// });
-
 app.listen(3000);
 console.log("Express server listening on 3000");
-
-// consider https://github.com/dstrek/node-browserify-express
